b535192cadc00c06d7ab2d3527845ae98e8ab403
helptic-sr <helptic@profe.colegiosanroque.org>
2026-09-22 07:21:26 +0200

    Add files via upload
    ..

---
+++ configuracion.js
@@ -0,0 +1,16 @@
+// Configuración del centro · generada con INSTALAR.html el 22/9/2026
+// Estos datos no son secretos: los necesita el navegador para conectar con Firebase.
+window.CONFIG_CENTRO = {
+  "firebase": {
+    "apiKey": "AIzaSyAzZT0M0EKI_lL8E6uqzY_Dl8tpCnFQN2A",
+    "authDomain": "incidencias-tic-sanroque.firebaseapp.com",
+    "projectId": "incidencias-tic-sanroque",
+    "storageBucket": "incidencias-tic-sanroque.firebasestorage.app",
+    "messagingSenderId": "992261516115",
+    "appId": "1:992261516115:web:959da29ae47eb093660fa6"
+  },
+  "dominio": "profe.colegiosanroque.org",
+  "proveedor": "google",
+  "tenant": "",
+  "recaptcha": ""
+};
